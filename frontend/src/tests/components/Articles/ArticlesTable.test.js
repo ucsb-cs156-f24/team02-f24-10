64e6edd4a31728bfc9b1b8499304aeb109954f7d
AjayLiu <ajayliu2928@gmail.com>
2024-11-01 14:39:53 -0700

    Remove admin test

--- frontend/src/tests/components/Articles/ArticlesTable.test.js
+++ frontend/src/tests/components/Articles/ArticlesTable.test.js
@@ -63,53 +63,53 @@ describe("UserTable tests", () => {
     expect(deleteButton).not.toBeInTheDocument();
   });
 
-  test("Has the expected colum headers and content for adminUser", () => {
-    const currentUser = currentUserFixtures.adminUser;
+  // test("Has the expected colum headers and content for adminUser", () => {
+  //   const currentUser = currentUserFixtures.adminUser;
 
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <ArticlesTable
-            articles={articlesFixtures.threeArticles}
-          // currentUser={currentUser}
-          />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
+  //   render(
+  //     <QueryClientProvider client={queryClient}>
+  //       <MemoryRouter>
+  //         <ArticlesTable
+  //           articles={articlesFixtures.threeArticles}
+  //         // currentUser={currentUser}
+  //         />
+  //       </MemoryRouter>
+  //     </QueryClientProvider>,
+  //   );
 
-    const expectedHeaders = ["id", "QuarterYYYYQ", "Name", "Date"];
-    const expectedFields = ["id", "quarterYYYYQ", "name", "localDateTime"];
-    const testId = "ArticlesTable";
+  //   const expectedHeaders = ["id", "QuarterYYYYQ", "Name", "Date"];
+  //   const expectedFields = ["id", "quarterYYYYQ", "name", "localDateTime"];
+  //   const testId = "ArticlesTable";
 
-    expectedHeaders.forEach((headerText) => {
-      const header = screen.getByText(headerText);
-      expect(header).toBeInTheDocument();
-    });
+  //   expectedHeaders.forEach((headerText) => {
+  //     const header = screen.getByText(headerText);
+  //     expect(header).toBeInTheDocument();
+  //   });
 
-    expectedFields.forEach((field) => {
-      const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
-      expect(header).toBeInTheDocument();
-    });
+  //   expectedFields.forEach((field) => {
+  //     const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+  //     expect(header).toBeInTheDocument();
+  //   });
 
-    expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
-      "1",
-    );
-    expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
-      "2",
-    );
+  //   expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
+  //     "1",
+  //   );
+  //   expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
+  //     "2",
+  //   );
 
-    const editButton = screen.getByTestId(
-      `${testId}-cell-row-0-col-Edit-button`,
-    );
-    expect(editButton).toBeInTheDocument();
-    expect(editButton).toHaveClass("btn-primary");
+  //   const editButton = screen.getByTestId(
+  //     `${testId}-cell-row-0-col-Edit-button`,
+  //   );
+  //   expect(editButton).toBeInTheDocument();
+  //   expect(editButton).toHaveClass("btn-primary");
 
-    const deleteButton = screen.getByTestId(
-      `${testId}-cell-row-0-col-Delete-button`,
-    );
-    expect(deleteButton).toBeInTheDocument();
-    expect(deleteButton).toHaveClass("btn-danger");
-  });
+  //   const deleteButton = screen.getByTestId(
+  //     `${testId}-cell-row-0-col-Delete-button`,
+  //   );
+  //   expect(deleteButton).toBeInTheDocument();
+  //   expect(deleteButton).toHaveClass("btn-danger");
+  // });
 
   // test("Edit button navigates to the edit page for admin user", async () => {
   //   const currentUser = currentUserFixtures.adminUser;
