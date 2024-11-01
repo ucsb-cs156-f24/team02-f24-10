535ff17f27201095a9ddb8c0655e9d33f93e3a13
r0ckwav3 <peterrockwave@gmail.com>
2024-10-31 18:04:35 -0700

    add fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizationFixtures = {
+  oneOrganization: [
+    {
+      orgCode: "SKY",
+      orgTranslationShort: "Skydiving Club",
+      orgTranslation: "Skydiving Club at UCSB",
+      inactive: false,
+    },
+  ],
+
+  threeOrganizations: [
+    {
+      orgCode: "SNU",
+      orgTranslationShort: "SIGMA NU",
+      orgTranslation: "SIGMA NU Fraternity",
+      inactive: true,
+    },
+    {
+      orgCode: "GDC",
+      orgTranslationShort: "UCSB GDC",
+      orgTranslation: "UCSB Game Development Club",
+      inactive: false,
+    },
+    {
+      orgCode: "ACM",
+      orgTranslationShort: "Assoc for Comp Mach",
+      orgTranslation: "Association for Computing Machinery",
+      inactive: false,
+    },
+  ],
+};
