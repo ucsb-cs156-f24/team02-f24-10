7e7b88c98d16f4e5e7d4a7754e920ff7240bb3d3
AjayLiu <ajayliu2928@gmail.com>
2024-11-01 17:03:40 -0700

    Wrong import, quick fix

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -6,7 +6,7 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import ArticlesCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
+import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 
 describe("ArticlesCreatePage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
