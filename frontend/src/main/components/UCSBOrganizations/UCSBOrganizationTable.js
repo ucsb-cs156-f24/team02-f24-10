a5a50ec46bee0fe2d786206675309279a417aa00
r0ckwav3 <peterrockwave@gmail.com>
2024-11-01 18:25:33 -0700

    supress warning for not using user in table

--- frontend/src/main/components/UCSBOrganizations/UCSBOrganizationTable.js
+++ frontend/src/main/components/UCSBOrganizations/UCSBOrganizationTable.js
@@ -53,12 +53,12 @@ export default function UCSBOrganizationTable({
     },
   ];
 
-//   if (hasRole(currentUser, "ROLE_ADMIN")) {
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
 //     columns.push(ButtonColumn("Edit", "primary", editCallback, testIdPrefix));
 //     columns.push(
 //       ButtonColumn("Delete", "danger", deleteCallback, testIdPrefix),
 //     );
-//   }
+  }
 
   return (
     <OurTable data={UCSBOrganizations} columns={columns} testid={testIdPrefix} />
