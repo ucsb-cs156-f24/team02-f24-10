b9fd8a15b969e072ae37a09cc69c6593047d0881
r0ckwav3 <peterrockwave@gmail.com>
2024-11-04 13:54:38 -0800

    add form, tests and stories for ucsb organizations

---
+++ frontend/src/main/components/UCSBOrganizations/UCSBOrganizationForm.js
@@ -0,0 +1,132 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+function UCSBOrganizationForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create",
+}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
+
+  const navigate = useNavigate();
+
+  const testIdPrefix = "UCSBOrganizationForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      {initialContents ? (
+        // we do have initial contents, so disable it and fill it in
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="orgCode">Organization Code</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "-orgCode"}
+            id="orgCode"
+            type="text"
+            {...register("orgCode")}
+            value={initialContents.orgCode}
+            disabled
+          />
+        </Form.Group>
+      ) : (
+        // we don't have initial contents, so validate it
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="orgCode">Organization Code</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "-orgCode"}
+            id="orgCode"
+            type="text"
+            {...register("orgCode", {
+              required: "Organization Code is required.",
+            })}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.orgCode?.message}
+          </Form.Control.Feedback>
+        </Form.Group>
+      )}
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="orgTranslationShort">
+          Organization Translation (Short)
+        </Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-orgTranslationShort"}
+          id="orgTranslationShort"
+          type="text"
+          isInvalid={Boolean(errors.orgTranslationShort)}
+          {...register("orgTranslationShort", {
+            required: "Organization Translation Short is required.",
+            maxLength: {
+              value: 15,
+              message: "Max length 15 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.orgTranslationShort?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="orgTranslation">
+          Organization Translation
+        </Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-orgTranslation"}
+          id="orgTranslation"
+          type="text"
+          isInvalid={Boolean(errors.orgTranslation)}
+          {...register("orgTranslation", {
+            required: "Organization Translation is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.orgTranslation?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="inactive">Inactive</Form.Label>
+        <Form.Select
+          data-testid={testIdPrefix + "-inactive"}
+          id="inactive"
+          type="text"
+          isInvalid={Boolean(errors.inactive)}
+          {...register("inactive", {
+            pattern: {
+              value: /true|false/,
+              message: "Inactive status is required.",
+            },
+          })}
+        >
+          <option>Select an option</option>
+          <option value="true">True</option>
+          <option value="false">False</option>
+        </Form.Select>
+        <Form.Control.Feedback type="invalid">
+          {errors.inactive?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid={testIdPrefix + "-cancel"}
+      >
+        Cancel
+      </Button>
+    </Form>
+  );
+}
+
+export default UCSBOrganizationForm;
---
+++ frontend/src/tests/components/UCSBOrganizations/UCSBOrganizationForm.test.js
@@ -0,0 +1,112 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import UCSBOrganizationForm from "main/components/UCSBOrganizations/UCSBOrganizationForm";
+import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ucsbDatesFixtures } from "fixtures/ucsbDatesFixtures";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("UCSBOrganizationForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = [
+    "Organization Code",
+    "Organization Translation (Short)",
+    "Organization Translation",
+    "Inactive",
+  ];
+  const testId = "UCSBOrganizationForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("renders correctly when passing in initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm
+            initialContents={ucsbOrganizationFixtures.oneOrganization}
+          />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Organization Code is required./);
+    expect(
+      screen.getByText(/Organization Translation Short is required./),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/Organization Translation is required./),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/Inactive status is required./),
+    ).toBeInTheDocument();
+
+    const nameInput = screen.getByTestId(`${testId}-orgTranslationShort`);
+    fireEvent.change(nameInput, { target: { value: "a".repeat(16) } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Max length 15 characters/)).toBeInTheDocument();
+    });
+  });
+});
