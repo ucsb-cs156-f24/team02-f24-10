c087170062423c0bfdadce30f0dfe0e36d9e75f5
Derrick Lee <d_d_lee@ucsb.edu>
2024-11-04 21:56:57 -0800

    fixed formatting

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,39 +1,38 @@
 const menuItemReviewFixtures = {
-    oneDate: {
-        id: 1,
-        itemId: 1,
-        reviewerEmail: "d_d_lee@ucsb.edu",
-        stars: 3,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "okay",
+  oneDate: {
+    id: 1,
+    itemId: 1,
+    reviewerEmail: "d_d_lee@ucsb.edu",
+    stars: 3,
+    dateReviewed: "2022-01-02T12:00:00",
+    comments: "okay",
+  },
+  threeDates: [
+    {
+      id: 1,
+      itemId: 1,
+      reviewerEmail: "d_d_lee0@ucsb.edu",
+      stars: 3,
+      dateReviewed: "2022-01-02T12:00:00",
+      comments: "okay",
     },
-    threeDates: [
-      {
-        id: 1,
-        itemId: 1,
-        reviewerEmail: "d_d_lee0@ucsb.edu",
-        stars: 3,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "okay",
-      },
-      {
-        id: 2,
-        itemId: 1,
-        reviewerEmail: "d_d_lee1@ucsb.edu",
-        stars: 4,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "great",
-      },
-      {
-        id: 3,
-        itemId: 1,
-        reviewerEmail: "d_d_lee2@ucsb.edu",
-        stars: 5,
-        dateReviewed: "2022-01-02T12:00:00",
-        comments: "amazing",
-      },
-    ],
-  };
-  
-  export { menuItemReviewFixtures };
-  
+    {
+      id: 2,
+      itemId: 1,
+      reviewerEmail: "d_d_lee1@ucsb.edu",
+      stars: 4,
+      dateReviewed: "2022-01-02T12:00:00",
+      comments: "great",
+    },
+    {
+      id: 3,
+      itemId: 1,
+      reviewerEmail: "d_d_lee2@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2022-01-02T12:00:00",
+      comments: "amazing",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
