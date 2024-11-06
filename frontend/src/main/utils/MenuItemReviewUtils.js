2befb75df39d9d948bfadae907461985205fb37d
Derrick Lee <d_d_lee@ucsb.edu>
2024-11-05 22:33:18 -0800

    stryker testing util fix

--- frontend/src/main/utils/MenuItemReviewUtils.js
+++ frontend/src/main/utils/MenuItemReviewUtils.js
@@ -1,9 +1,13 @@
 import { toast } from "react-toastify";
 
+// Stryker disable all : dev facing debug function, no need to test
+
 export function onDeleteSuccess(message) {
   console.log(message);
   toast(message);
 }
+
+// Stryker restore all
 
 export function cellToAxiosParamsDelete(cell) {
   return {
