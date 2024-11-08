5e16d6dbb51f1ffcc86bca53bb1c04b199529773
Rajvir Ranu <rajvir.ranu23@gmail.com>
2024-11-07 17:44:32 -0800

    Wrote fixtures for helpRequests

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,41 @@
+const helpRequestsFixtures = {
+ oneReq: {
+    id: 1,
+    requesterEmail: "foo@bar",
+    teamId: "foofoo",
+    tableOrBreakoutRoom: "foo2",
+    requestTime: "2020-12-25T00:00:00",
+    explanation: "ground control to major tom",
+    solved: true,
+ },
+ threeReqs: [
+  {
+    id: 1,
+    requesterEmail: "foo@bar",
+    teamId: "foofoo",
+    tableOrBreakoutRoom: "foo2",
+    requestTime: "2020-12-25T00:00:00",
+    explanation: "ground control to major tom",
+    solved: true,
+  },
+  {
+    id: 2,
+    requesterEmail: "bar@foo.com",
+    teamId: "barbar",
+    tableOrBreakoutRoom: "bar3",
+    requestTime: "2020-12-25T00:00:00",
+    explanation: "major tom to ground control",
+    solved: false,
+  },
+  {
+    id: 3,
+    requesterEmail: "foofoo@bar.com",
+    teamId: "barbar",
+    tableOrBreakoutRoom: "foo5",
+    requestTime: "1990-05-15T05:00:00",
+    explanation: "time-travel issues",
+    solved: false,
+  },
+ ],
+};
+export { helpRequestsFixtures };
