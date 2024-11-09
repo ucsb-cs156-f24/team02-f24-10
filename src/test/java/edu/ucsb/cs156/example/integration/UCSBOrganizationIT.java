f8f969d730d0fdede35aa6ed9b9baef292cd2bf3
r0ckwav3 <peterrockwave@gmail.com>
2024-11-08 16:42:50 -0800

    remove unused imports; fix tab size; format document

--- src/test/java/edu/ucsb/cs156/example/integration/UCSBOrganizationIT.java
+++ src/test/java/edu/ucsb/cs156/example/integration/UCSBOrganizationIT.java
@@ -1,12 +1,8 @@
 package edu.ucsb.cs156.example.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,11 +23,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
-import edu.ucsb.cs156.example.entities.Restaurant;
 import edu.ucsb.cs156.example.entities.UCSBOrganization;
-import edu.ucsb.cs156.example.repositories.RestaurantRepository;
 import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
 import edu.ucsb.cs156.example.repositories.UserRepository;
 import edu.ucsb.cs156.example.services.CurrentUserService;
@@ -45,59 +38,59 @@ import edu.ucsb.cs156.example.testconfig.TestConfig;
 @Import(TestConfig.class)
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UCSBOrganizationIT {
-    @Autowired
-    public CurrentUserService currentUserService;
+        @Autowired
+        public CurrentUserService currentUserService;
 
-    @Autowired
-    public GrantedAuthoritiesService grantedAuthoritiesService;
+        @Autowired
+        public GrantedAuthoritiesService grantedAuthoritiesService;
 
-    @Autowired
-    UCSBOrganizationRepository ucsbOrganizationRepository;
+        @Autowired
+        UCSBOrganizationRepository ucsbOrganizationRepository;
 
-    @Autowired
-    public MockMvc mockMvc;
+        @Autowired
+        public MockMvc mockMvc;
 
-    @Autowired
-    public ObjectMapper mapper;
- 
-    @MockBean
-    UserRepository userRepository;
+        @Autowired
+        public ObjectMapper mapper;
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_user_can_get_all_ucsborganizations() throws Exception {
-        // arrange
+        @MockBean
+        UserRepository userRepository;
 
-        ArrayList<UCSBOrganization> expectedOrgs = new ArrayList<>();
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_user_can_get_all_ucsborganizations() throws Exception {
+                // arrange
 
-        UCSBOrganization skydiving = UCSBOrganization.builder()
-                .orgCode("SKY")
-                .orgTranslationShort("SKYDIVING CLUB")
-                .orgTranslation("SKYDIVING CLUB AT UCSB")
-                .inactive(false)
-                .build();
+                ArrayList<UCSBOrganization> expectedOrgs = new ArrayList<>();
 
-        ucsbOrganizationRepository.save(skydiving);
-        expectedOrgs.add(skydiving);
+                UCSBOrganization skydiving = UCSBOrganization.builder()
+                                .orgCode("SKY")
+                                .orgTranslationShort("SKYDIVING CLUB")
+                                .orgTranslation("SKYDIVING CLUB AT UCSB")
+                                .inactive(false)
+                                .build();
 
-        UCSBOrganization sigmanu = UCSBOrganization.builder()
-                .orgCode("SNU")
-                .orgTranslationShort("SIGMA NU")
-                .orgTranslation("SIGMA NU FRATERNITY")
-                .inactive(true)
-                .build();
+                ucsbOrganizationRepository.save(skydiving);
+                expectedOrgs.add(skydiving);
 
-        ucsbOrganizationRepository.save(sigmanu);
-        expectedOrgs.add(sigmanu);
+                UCSBOrganization sigmanu = UCSBOrganization.builder()
+                                .orgCode("SNU")
+                                .orgTranslationShort("SIGMA NU")
+                                .orgTranslation("SIGMA NU FRATERNITY")
+                                .inactive(true)
+                                .build();
 
-        // act
-        MvcResult response = mockMvc.perform(get("/api/ucsborganizations/all"))
-                .andExpect(status().isOk()).andReturn();
+                ucsbOrganizationRepository.save(sigmanu);
+                expectedOrgs.add(sigmanu);
 
-        // assert
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsborganizations/all"))
+                                .andExpect(status().isOk()).andReturn();
 
-        String expectedJson = mapper.writeValueAsString(expectedOrgs);
-        String responseString = response.getResponse().getContentAsString();
-        assertEquals(expectedJson, responseString);
-    }
+                // assert
+
+                String expectedJson = mapper.writeValueAsString(expectedOrgs);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
 }
