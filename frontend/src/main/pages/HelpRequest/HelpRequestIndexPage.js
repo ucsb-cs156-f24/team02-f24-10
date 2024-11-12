b3962cfb278ee4abf9bd7430458e1f214ec6d664
Rajvir Ranu <rajvir.ranu23@gmail.com>
2024-11-11 20:22:44 -0800

    Revert "Fixed placeholder index page, made real create page"
    This reverts commit 944911a742ef68795b0ad808104d773ffb3fbf15.

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -7,10 +7,10 @@ export default function HelpRequestIndexPage() {
       <div className="pt-2">
         <h1>Index page not yet implemented</h1>
         <p>
-          <a href="/helprequests/create">Create</a>
+          <a href="/placeholder/create">Create</a>
         </p>
         <p>
-          <a href="/helprequests/edit/1">Edit</a>
+          <a href="/placeholder/edit/1">Edit</a>
         </p>
       </div>
     </BasicLayout>
